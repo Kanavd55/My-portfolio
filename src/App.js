c5e2d91bcb6c119e2ecc9b92ec36ab8fcac94b8a
Kanav Dahat <kanavdahat55@gmail.com>
2023-09-19 21:26:01 +0530

    Navbar component is created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-slate-500">Tailwind is configured</h1>
+      <Navbar/>
     </div>
   );
 }
