687aee74de13bd7ee4936f8315b5c70e0077ba1e
Kanav Dahat <kanavdahat55@gmail.com>
2023-09-23 21:06:08 +0530

    Youtube Video watch link added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,6 +13,7 @@ const Projects = () => {
         about="Foodie's Spot is a responsive food ordering web app which uses Swiggy's live public api data to fetch restaurant details.It has shimmer UI for better user experience, conditional rendering,react top loader bar,lazy loading and react-hot-toast for notifications."
         github_link="https://github.com/Kanavd55/Food-Ordering-App/"
         live_link="https://foodiespot-kanav.netlify.app/"
+        video_link="https://www.youtube.com/watch?v=_lgWKtnEho4"
       />
       <ProjectCard
       img_url="/CineUniverse.png"
@@ -20,6 +21,7 @@ const Projects = () => {
         about="CineUniverse: Your go-to source for trending movies, top-rated films, and hot TV shows. Explore, search, and watch trailers effortlessly with our user-friendly interface, powered by the reliable TMDB API. Dive into the world of entertainment today!"
         github_link="https://github.com/Kanavd55/CineUniverse"
         live_link="https://cineuniverse-kanav.netlify.app/"
+        video_link="https://www.youtube.com/watch?v=d5K4lEkuJik&t=93s"
       />
     </div>
     </div>
